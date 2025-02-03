8f1d1b5a92b911e0e8ae284236e11831dc4bdee0
Amanda Oliveira <amandaol7b@gmail.com>
2025-02-03 18:34:21 -0300

    feat: adição do footer e da navbar

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return (
+      <>
+        <div className="flex gap-12 justify-center">
+          <img src="/img/x.svg" alt="Link para o Twitter" />
+          <img src="/img/instagram.svg" alt="Link para o Instagram" />
+          <img src="/img/github.svg" alt="Link para o Github" />
+          <img src="/img/behance.svg" alt="Link para o Behance" />
+          <img src="/img/linkedin.svg" alt="Link para o Linkedin" />
+        </div>
+      </>
+    );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+export default function Navbar(){
+    return (
+      <>
+        <div className="flex justify-between">
+          <div className="flex items-center gap-2">
+            <img
+              className="md:w-12 w-11"
+              src="/img/amanda.png"
+              alt="Selfie de perfil da Amanda"
+            />
+            <p className="font-bold">Amanda Oliveira</p>
+            <img
+              className="md:w-auto w-5"
+              src="/img/verified.svg"
+              alt="Icone de verificado"
+            />
+          </div>
+          <div className="flex items-center md:gap-6 gap-3">
+            <img
+              className="md:w-auto w-12"
+              src="/img/dark.svg"
+              alt="Icone para alterar para light mode"
+            />
+            <img
+              className="md:w-auto w-12 rotate-90"
+              src="/img/menu.svg"
+              alt="Icone para acessar o menu de opções"
+            />
+          </div>
+        </div>
+      </>
+    );
+}
